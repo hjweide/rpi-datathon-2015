ad85b2e487d4758d114adfdaac28849555a15168
Hendrik Weideman <weideh@rpi.edu>
2015-04-25 16:10:56 -0400

    predict_sentiment.py

--- predict_sentiment.py
+++ predict_sentiment.py
@@ -19,13 +19,12 @@ def predict_sentiment_posts(thread_list, model, vectorizer):
         pred = model.predict(data_features)
 
         if len(comments) > 0:
-            #title_weight = 0.1
-            #title_sentiment = title_weight * pred[0]
-            #non_title_sentiment = (1 - title_weight) * np.mean(pred[1:])
-            title_sentiment = 0
-            non_title_sentiment = np.mean(pred)
+            title_weight = 0.1
+            title_sentiment = title_weight * pred[0]
+            non_title_sentiment = (1 - title_weight) * np.mean(pred[1:])
+            #title_sentiment = 0
+            #non_title_sentiment = np.mean(pred)
         else:
-            continue
             title_sentiment = pred[0]
             non_title_sentiment = 0
 
@@ -41,7 +40,7 @@ if __name__ == '__main__':
     with open(join('data', 'vectorizer.pickle'), 'rb') as ifile:
         vectorizer = pickle.load(ifile)
 
-    university_names = ['mit', 'usc', 'uiuc', 'rpi']
+    university_names = ['mit', 'usc', 'uiuc', 'rpi', 'rit', 'uwaterloo']
     for university in university_names:
         print('processing %s' % university)
         with open(join('data', '%s.pickle' % university)) as ifile:
