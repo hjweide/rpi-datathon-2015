20ce51635c776b9aba023326659e8755e72e63d9
Hendrik Weideman <weideh@rpi.edu>
2015-04-25 15:07:38 -0400

    Saving vectorizer.

--- learn_tweets.py
+++ learn_tweets.py
@@ -13,7 +13,7 @@ from time import time
 from utils import read_tweets
 
 
-def learn_sentiment_from_tweets(clean_tweets, clean_tweets_sentiments, modelfile, retrain=False):
+def learn_sentiment_from_tweets(clean_tweets, clean_tweets_sentiments, modelfile, vectorfile, retrain=False):
     vectorizer = CountVectorizer(analyzer='word',
                                  tokenizer=None,
                                  preprocessor=None,
@@ -41,12 +41,14 @@ def learn_sentiment_from_tweets(clean_tweets, clean_tweets_sentiments, modelfile
         forest = forest.fit(train_data, train_labels)
         print('training completed in %.2f' % (time() - t0))
         print('saving model to %s' % (modelfile))
-        with open(modelfile, 'wb') as ofile:
-            pickle.dump(forest, ofile)
+        with open(modelfile, 'wb') as mfile, open(vectorfile, 'wb') as vfile:
+            pickle.dump(forest, mfile)
+            pickle.dump(vectorizer, vfile)
     else:
         print('loading random forest...')
-        with open(modelfile, 'rb') as ifile:
-            forest = pickle.load(ifile)
+        with open(modelfile, 'rb') as mfile, open(vectorfile, 'wb') as vfile:
+            forest = pickle.load(mfile)
+            vectorizer = pickle.load(vfile)
 
     print('predicting on training set...')
     train_pred = forest.predict(train_data)
@@ -64,11 +66,12 @@ if __name__ == '__main__':
     datafile = join(root, 'data', 'tweets_clean.csv')
     tweetsfile = join(root, 'data', 'tweets_clean.pickle')
     modelfile = join(root, 'data', 'model.pickle')
+    vectorfile = join(root, 'data', 'vectorizer.pickle')
     clean_tweets, clean_tweets_sentiments = read_tweets(datafile, tweetsfile)
 
     clean_tweets = np.array(clean_tweets)
     clean_tweets_sentiments = np.array(clean_tweets_sentiments)
 
-    num_tweets = 10000
+    num_tweets = 5000
     random_indices = np.random.choice(clean_tweets.shape[0], size=num_tweets, replace=False)
-    learn_sentiment_from_tweets(clean_tweets[random_indices], clean_tweets_sentiments[random_indices], modelfile, retrain=True)
+    learn_sentiment_from_tweets(clean_tweets[random_indices], clean_tweets_sentiments[random_indices], modelfile, vectorfile, retrain=True)
